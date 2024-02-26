67202038119b6354555221d2dc0a3b19fd14dfc5
sacden <sacden2009@gmail.com>
2024-02-26 22:21:01 +0100

    init: add basic template

---
+++ App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {SafeAreaView, View, StatusBar, StyleSheet, Text} from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <StatusBar />
+      <View style={styles.container}>
+        <Text>Test</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;
